fa4de04ec50ebe6ebab29d59e57befc5438dfed2
vangmaya0327-collab <vangmaya0327@gmail.com>
2026-08-19 12:00:06 +0530

    Add files via upload

---
+++ HierarchicalInheritance .java
@@ -0,0 +1,26 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal Eats");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows");
+    }
+}
+public class HierarchicalInheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+
+        Cat c = new Cat();
+        c.eat();
+        c.meow();
+    }
+}

98ed79b129e18d5da7149471d8f59bb036d2243d
vangmaya0327-collab <vangmaya0327@gmail.com>
2026-08-19 12:00:50 +0530

    Add files via upload

---
+++ ExceptionHandling .java
@@ -0,0 +1,15 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+        try {
+            int result = a / b;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Finally block executed");
+        }
+    }
+}
